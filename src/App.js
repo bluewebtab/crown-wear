2b45d88d221d7bc5ea537d51f11e00244f99bc2e
Ronny Gutierrez <ronny.icne@gmail.com>
2020-06-16 06:50:43 -0700

    Modified setState in App.js

--- src/App.js
+++ src/App.js
@@ -32,9 +32,10 @@ class App extends React.Component {
            }
           })
         })
+      }else{
+        this.setState({currentUser: userAuth})
       }
 
-      this.setState({currentUser: userAuth})
     })
   }
 
