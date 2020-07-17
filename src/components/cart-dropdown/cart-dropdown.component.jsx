e0158de42854a2dbc2b93b24945833cfe3b2bf87
Ronny Gutierrez <ronny.icne@gmail.com>
2020-07-17 10:56:11 -0700

    moved our directory and shop data into its own reducer, created respective selectors and updataed components with redux flow for directory/shop

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
-
+import { createStructuredSelector } from 'reselect';
+import {withRouter} from 'react-router-dom';
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
 import {selectCartItems} from '../../redux/cart/cart.selectors';
@@ -8,8 +9,6 @@ import {toggleCartHidden} from '../../redux/cart/cart.actions';
 
 
 import './cart-dropdown.styles.scss'
-import { createStructuredSelector } from 'reselect';
-import {withRouter} from 'react-router-dom';
 
 const CartDropdown = ({cartItems, history, dispatch}) => (
     <div className='cart-dropdown'>
@@ -28,7 +27,7 @@ const CartDropdown = ({cartItems, history, dispatch}) => (
             }
         </div>
             <CustomButton onClick={() => {
-               history.push('/checkout');
+               history.push("/checkout");
                dispatch(toggleCartHidden())
             }}>
                GO TO CHECKOUT
